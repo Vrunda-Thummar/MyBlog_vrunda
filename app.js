0614624bb413a29c4d7f96d68a16871a28f9ab32
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-09-02 18:44:51 +0530

    02-09-2024

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/indexRoutes'));
+app.use('/', require('./routes/authRoutes'));
+app.use('/', require('./routes/blogRoutes'));
+
+// Start server
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
